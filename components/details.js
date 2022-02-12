0abc79bd6ea5b3ee31a9afe0e5617cd76006f39a
Neslyn <neslynchellaiah@gmail.com>
2022-02-12 17:09:10 +0530

    Details page

--- components/details.js
+++ components/details.js
@@ -1,20 +1,31 @@
 import { useState } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import { Input } from "react-native-elements";
-import { color } from "react-native-elements/dist/helpers";
 
 const Details = () =>{
-    const [category, setCategory] = useState();
     return (
         <View style={styles.screen}>
             <Text style={styles.boldText}>Expense</Text>
             <Input 
             label="Category" 
             style={styles.inputField} 
-            labelStyle={styles.input} 
+            labelStyle={styles.label} 
             inputStyle={styles.input} 
-            inputContainerStyle={{borderBottomColor: "#ffffff"}}
-            containerStyle={styles.spacer}/>
+            inputContainerStyle={{borderBottomColor: "#ffffff"}}/>
+            <Input 
+            label="Amount" 
+            style={styles.inputField} 
+            labelStyle={styles.label} 
+            inputStyle={styles.input} 
+            inputContainerStyle={{borderBottomColor: "#ffffff"}}/>
+            <Input 
+            multiline
+            label="Description" 
+            style={styles.inputField} 
+            labelStyle={styles.label} 
+            inputStyle={styles.input} 
+            inputContainerStyle={{borderBottomColor: "#ffffff"}}/>
+            
         </View>
     )
 }
@@ -34,10 +45,12 @@ const styles = StyleSheet.create({
         marginBottom: 25
     },
     input: {
-        color: '#ffffff'
+        color: '#ffffff',
+        fontSize: 14
     },
-    spacer: {
-        marginTop: 5,
+    label: {
+        color: '#bfbfbf',
+        fontSize: 12
     }
 });
 
